f8c95c82dd3cfcb8e115e6272e13c76b5a74ea0c
Lamp <dmvoicestudios@gmail.com>
2019-03-19 09:58:25 +0100

    Fixed Manhattan distance to always work

--- Seminar1.py
+++ Seminar1.py
@@ -7,12 +7,13 @@ start = time.time()
 if __name__ == '__main__':
     file = open(sys.argv[1], "r")
     fen0 = [line for line in file][0]
+    fen0s = fen0.split(" ")
     avmoves=int(fen0.split(" ")[-1])
-    startfen=fen0.split(" ")[-3]+" "+fen0.split(" ")[-2]+" "+" - - 0 0"
+    startfen=fen0s[-3]+" "+fen0s[-2]+" "+" - - 0 0"
     board=chess.Board(startfen)
 
     # Check who plays black or white
-    color=fen0.split(" ")[1]
+    color=fen0s[1]
     moveside = False
     if color == "w":
         moveside = True
@@ -21,34 +22,42 @@ if __name__ == '__main__':
     #print(startfen)
 
     def rate(move,current):
-        score=1000 + random.randint(1,10)
+        score=1000 #+ random.randint(1,10)
         avmovs=current[1]
         board=current[0]
 
         # If move is a capture reward it
-        if board.is_capture(move):
-            score-=50
+        #if board.is_capture(move):
+        #    score -= 50
         # If move is irreversible reward it
-        if board.is_irreversible(move):
-            score -= 20
+        #if board.is_irreversible(move):
+            #score -= 20
 
         # Reward Manhattan distance to the King
-            ms = move.uci()[:2]
-            # Name of file
-            kf = chess.square_file(board.king(enemyside))
-            mf = int([a for a in range(len(chess.FILE_NAMES)) if chess.FILE_NAMES[a] == ms[0]][0])
+        ms = move.uci()[:2]
+        # Name of file
+        kf = chess.square_file(board.king(enemyside))
+        mf = int([a for a in range(len(chess.FILE_NAMES)) if chess.FILE_NAMES[a] == ms[0]][0])
 
-            # Number of rank
-            kr = chess.square_rank(board.king(enemyside))
-            mr = int(ms[1])-1
-            md = abs(kf-mf)+abs(kr-mr)
-            score+= md * 100
+        # Number of rank
+        kr = chess.square_rank(board.king(enemyside))
+        mr = int(ms[1])-1
+        md = abs(kf-mf)+abs(kr-mr)
+        score+= md * 100
 
         return score
 
     def BFS_move(brd, c):
+
+
+
         nextq = deque([(brd, c, 0)])
         while nextq:
+            if (float(time.time()) - float(start)) > 20:
+                #print("Took to long " + str((float(time.time()) - float(start))))
+                #break
+                pass
+
             #print(len(nextq))
             nextq = deque(sorted(list(nextq), key=lambda x: x[2]))
             current = nextq.popleft()
